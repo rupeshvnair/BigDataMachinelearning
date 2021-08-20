4903028f367777683c29de0aa3a117b9b7ff6455
Rupesh Viswanadhan <89230075+rupeshvnair@users.noreply.github.com>
2021-08-20 08:38:39 +0530

    Update Map1.py
    made a change in comment section

--- Map1.py
+++ Map1.py
@@ -10,8 +10,5 @@ for line in sys.stdin:
     # increase counters
     for word in words:
         # write the results to STDOUT (standard output);
-        # what we output here will be the input for the
-        # Reduce step, i.e. the input for reducer.py
-        #
-        # tab-delimited; the trivial word count is 1
+        # this code was modified by Rupesh
         print (word)
